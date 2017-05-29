832674a4ab4436cf190af152d1ef4582d8aab7cd
AntoineBenard76 <antoine.benard@uha.fr>
2017-05-29 17:25:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-var nom = "Nolwenn";
-alert(nom);
+//var prenom = prompt("Donnez la somme en euros :");
+//var nom = prompt ("quel est ton nom ?");
+//alert("Vous vous appelez " + prenom);
+function conversion() {
+    var somme = prompt("Donnez la somme en euros :");
+    var resultat = somme * 6.55957;
+    alert(somme + "E\n" + resultat + " F");
+    }
