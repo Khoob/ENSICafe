49f33a612d9d088cdfb6abbd9731a9f5212c3e63
AntoineBenard76 <antoine.benard@uha.fr>
2017-05-29 15:40:32 +0200

    Les premiers pop up ! :D

--- header.php
+++ header.php
@@ -8,8 +8,14 @@
 <body>
 	<header>
 			<h1>
+            <img id="img_droite" src="img/logo.jpg" width="50px" height="50px" title="logo-ensiscafe" alt="ENSICafe" />
 			<a href="index.html">ENSICafé</a>
+            <img id="img_guache" src="img/logo_ensisa.jpg" width="50px" height="50px" title="logo-ensisa" alt="ENSISA" />
 			</h1>
-			<nav>
-			<a href="Login.html">Connexion</a>
+         
+			<!--<nav>
+			<a href="Login.html">Connexion</a>-->
+             <?php
+            include('navigation.php'); 
+        ?>
 	</header>
--- index.php
+++ index.php
@@ -1,9 +1,15 @@
-<?php include('header.php');?>
-
-			<h3 id="login">
+<?php
+	include("header.php");
+?>
+<a href="#" onclick="alert('Bonjour étranger!');"<p> Cette phrase de test est vraiment géniale !</p></a>
+<!-- Page de connection			
+            <h3 id="login">
 			<a href="Login.html">Connexion</a>
 			</h3>
 			<h3 id="creerCompte">
 			<a href="creerCompte.html">Créer votre compte</a>
 			</h3>
-<?php include('footer.php');?>
+-->
+<?php
+	include("footer.php");
+?>
