5d14930849ffac25df6c88c5c6b308fba5dc483f
Thomas Blangero <chungaishaeve@gmail.com>
2017-05-29 11:26:00 +0200

    head contient le head de index.php

---
+++ head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+<link rel='stylesheet' href='style.css' >
+<meta charset="utf-8" />
+<title>ENSICafé</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	include("head.php")
+?>
+	<header>
+			<h1>
+			<a href="index.html">ENSICafé</a>
+			</h1>
+	</header>
+			<h3 id="login">
+			<a href="Login.html">Connexion</a>
+			</h3>
+			<h3 id="creerCompte">
+			<a href="creerCompte.html">Créer votre compte</a>
+			</h3>
+</body>
+</html>
