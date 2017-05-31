c21a7b3f0b26cf2b3f5a0733c490b7a3e908de72
Thomas Blangero <chungaishaeve@gmail.com>
2017-05-31 11:41:09 +0200

    ajout du test de vérification et redirection

--- traitementLogin.php
+++ traitementLogin.php
@@ -1,13 +1,27 @@
 <?php
-	$lmail=$bdd->query('SELECT mail FROM profil');
-	$lmdp=$bdd->query('SELECT mdp FROM profil');
+	// la fonction de redirection ------------ 
+	function redir($url){ 
+		echo "<script language=\"javascript\">"; 
+		echo "window.location='$url';"; 
+		echo "</script>"; 
+	} 
+	$reponse=$bdd->query('SELECT mail,mdp FROM profil');
 	include('header.php');
 	if(isset($_POST['envoi'])){
 		if(!empty($_POST['mel'])){
 			$mel=$_POST['mail'];
 			if(!empty($_POST['mdp'])){
 				$mdp=$_POST['mdp'];
-				
+				foreach($reponse as $element){
+					if($element['mail']==$mel && $element['mdp']==$mdp){	
+					// Utiliser la redirection --------------- 
+					redir("accueil.php"); 
+					}
+					else{
+						echo "Vous vous êtes trompés de mail ou de mot de passe";
+						redir("Login.php")
+					}
+				}
 					
 			}
 		}
