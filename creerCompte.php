3a16e6b0d09c299bfab01125e9457301eeffc0c4
Thomas Blangero <chungaishaeve@gmail.com>
2017-05-31 09:34:14 +0200

    Ajout du bouton pour envoyer

--- creerCompte.php
+++ creerCompte.php
@@ -32,6 +32,9 @@
 		<label for="mdp">Votre mot de passe</label>
 		<input type="text" name="mdp" placeholder="Votre mot de passe"/>
 		</p>
+		<p>
+		<input type="submit" name="envoi" value="S'inscrire"/>
+		</p>
 		</form>
 		</section>
 <?php
