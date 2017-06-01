c7c33e0405e618311f9e4d631948c7b86f12f89c
Thomas Blangero <chungaishaeve@gmail.com>
2017-06-01 08:38:20 +0200

    Add files via upload

--- traitementCreerCompte.php
+++ traitementCreerCompte.php
@@ -15,7 +15,7 @@
 								$fonction=$_POST['fonction'];
 								if(!empty($_POST['mdp'])){
 									$mdp=$_POST['mdp'];
-									echo "<p> test: ".$mel." ".$date." ".$genre." ".$fonction.
+									echo "<p> test: ".$mel." ".$date." ".$genre." ".$fonction."</p>";
 									echo"<p>Félicitation ".$nom." ".$prenom.", vous vous êtes bien inscrit sur ENSICafé.</p>";
 									$insertion=$bdd->prepare('INSERT INTO profil VALUES(NULL,"'.$nom.'","'.$prenom.'","'.$mel.'","'.$date.'","'.$genre.'","'.$fonction.'","'.$mdp.'")');
 									$insertion->execute();
