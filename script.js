d385e2ff32f9cb472d1dcf2a048f29697f3772d0
daas96 <saadbennani96@gmail.com>
2017-06-01 13:36:14 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-//var prenom = prompt("Donnez la somme en euros :");
-//var nom = prompt ("quel est ton nom ?");
-//alert("Vous vous appelez " + prenom);
-function conversion() {
-    var somme = prompt("Donnez la somme en euros :");
-    var resultat = somme * 6.55957;
-    alert(somme + "E\n" + resultat + " F");
-    }
