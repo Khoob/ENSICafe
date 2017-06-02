31796a08a15e9b37ee3b897266afb7300dc8d729
Olivier <olivier.tinh@gmail.com>
2017-06-02 14:36:40 +0200

    PHP files

---
+++ navigation.php
@@ -0,0 +1,17 @@
+<!-- Navbar : barre de navigation en haut -->
+<nav class="navbar navbar-inverse navbar-static-top">
+    <div class="container-fluid">
+        <div class="col-sm-9">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"><h1>ensicafé <small>le réseau social dopé à la caféine</small></h1></a>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div>
+                <ul class="nav navbar-nav pull-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-check"></span> Se déconnecter</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
