0d63dacf91b4d4e5206ffea39f442cab09477012
Thomas Blangero <chungaishaeve@gmail.com>
2017-06-07 10:50:37 +0200

    Pour ajouter des images
    à partir d'une url ou d'un fichier.
    La base de données qui va avec.

---
+++ testImage.php
@@ -0,0 +1,91 @@
+<?php
+try{
+	$bdd=new PDO('mysql:host=localhost;dbname=testImage;charset=utf8','root','');
+	}
+	catch(Exception $e){
+		die('Erreur :'.$e->getMessage());
+	}
+	$dossier = 'img/';
+if(is_dir($dossier)){
+} else{
+   mkdir($dossier);
+}
+?>
+<form enctype="multipart/form-data" method="POST">
+	<label>Mettez l'url de l'image ou choississez votre fichier</label>
+	<input type="file" name="avatar" />
+	<input type="text" name="image" placeholder="l'url de l'image" />
+	<input type="hidden" name="MAX_FILE_SIZE" value="100000000" />
+	<input type="submit" name="upload" value="Envoyer" />
+</form>
+<?php
+if(isset($_POST['upload'])){
+	$fichier = basename($_FILES['avatar']['name']);
+	$taille_maxi = 100000000;
+	$taille = filesize($_FILES['avatar']['tmp_name']);
+	$extensions = array('.png', '.gif', '.jpg', '.jpeg');
+	$extension = strrchr($_FILES['avatar']['name'], '.'); 
+	//Début des vérifications de sécurité...
+	if(!in_array($extension, $extensions) && empty($_POST['image'])) //Si l'extension n'est pas dans le tableau
+	{
+		 $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+	}
+	if($taille>$taille_maxi && empty($_POST['image']))
+	{
+		 $erreur = 'Le fichier est trop gros...';
+	}
+	if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+	{
+		 //On formate le nom du fichier ici...
+		 $fichier = strtr($fichier, 
+			  'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+			  'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+		 $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+		 if(move_uploaded_file($_FILES['avatar']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+		 {
+			  echo 'Upload effectué avec succès !';
+		 }
+		 else //Sinon (la fonction renvoie FALSE).
+		 {
+			  echo 'Echec de l\'upload !';
+		 }
+	}
+	if(!empty($erreur))
+	{
+		 echo $erreur;
+	}
+	if(!empty($fichier)){
+		$image = htmlspecialchars($fichier);
+		$stockage="disque";
+		echo " test ";
+		$insertion = $bdd->prepare('INSERT INTO image(url,stockage) VALUES(?,?)');
+		$insertion->execute(array($fichier,$stockage));
+		if(!empty($insertion)){
+			print_r($bdd->errorInfo());
+		}
+	}
+	if(!empty($_POST['image'])){
+		$image2 = htmlspecialchars($_POST['image']);
+		$stockage="url";
+		echo " test 2 ";
+		$insertion2 = $bdd->prepare('INSERT INTO image(url,stockage) VALUES(?,?)');
+		$insertion2->execute(array($image2,$stockage));
+		if(!empty($insertion2)){
+			print_r($bdd->errorInfo());
+		}
+	}
+}
+?>
+<div>
+	<?php
+		$image=$bdd->query("SELECT url,stockage from image");
+		foreach($image as $im){
+			if($im['stockage']=="url"){
+				echo '<img src="'.$im['url'].'" alt="une image" width="400" height="400"/>';
+			}
+			else{
+				echo '<a href="'.$dossier.$im['url'].'"" ><img src="'.$dossier.$im['url'].'" alt="une image" width="400" height="400"/></a>';
+			}
+		}
+	?>
+</div>
---
+++ testImage.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 07 juin 2017 à 10:50
+-- Version du serveur :  5.7.17
+-- Version de PHP :  5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `testImage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `image`
+--
+
+CREATE TABLE `image` (
+  `id` int(11) NOT NULL,
+  `url` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
+  `stockage` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `image`
+--
+
+INSERT INTO `image` (`id`, `url`, `stockage`) VALUES
+(1, 'https://media.senscritique.com/media/000006491241/source_big/Gurren_Lagann.jpg', 'url'),
+(2, 'http://www.nautiljon.com/images/anime/00/76/steins_gate_1967.jpg?1446742749', 'url'),
+(3, 'https://vignette3.wikia.nocookie.net/neptunia/images/2/24/4GO_Slider.jpg/revision/latest/scale-to-width-down/670?cb=20161101031831', 'url'),
+(7, 'http://galerie.nautiljon.com/07/84/medaka_box_639948.jpg', 'url'),
+(11, 'http://blog.textile-alsace.com/wp-content/uploads/2014/11/ensisa-mulhouse-logo.jpeg', 'url'),
+(14, '1a4d354e387795ab6212fb47e3bb00e3.jpg', 'disque'),
+(15, 'http://hdqwalls.com/download/dva-overwatch-artwork-lu-2048x1152.jpg', 'url'),
+(16, '9a80866e4f9878f3b710d4e1ab8d05a1.png', 'disque'),
+(17, '0a2f67619fd8041da4946f2e5ed844fe.jpg', 'disque'),
+(18, 'https://www.jvfrance.com/wp-content/uploads/2015/06/vanquish_large.jpeg', 'url');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `video`
+--
+
+CREATE TABLE `video` (
+  `id` int(11) NOT NULL,
+  `url` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
+  `stockage` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `image`
+--
+ALTER TABLE `image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `video`
+--
+ALTER TABLE `video`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `image`
+--
+ALTER TABLE `image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+--
+-- AUTO_INCREMENT pour la table `video`
+--
+ALTER TABLE `video`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
